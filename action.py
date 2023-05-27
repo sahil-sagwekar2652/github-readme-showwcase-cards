f00552e5953a31bfef8f1da13aacb0aed43be1c8
sahil-sagwekar2652 <sahilss2652@gmail.com>
2023-05-27 21:12:34 +0530

    create flask file structure

--- action.py
+++ action.py
@@ -6,7 +6,7 @@ import os
 load_dotenv()
 
 # fixed url
-url = "https://cache.showwcase.com/user/" + "sahil-sagwekar2652"
+url = "https://cache.showwcase.com/user/" + "atapas398"
 
 # API Headers
 headers = {'Authorization': 'Bearer ' + os.getenv('API_KEY')}
@@ -15,4 +15,6 @@ headers = {'Authorization': 'Bearer ' + os.getenv('API_KEY')}
 # API Request
 response = requests.get(url, headers=headers)
 
-print(response.text)
+with open("output.txt", 'w') as file:
+	file.write(response.text)
+	file.close()
